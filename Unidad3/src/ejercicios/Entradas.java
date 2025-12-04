44e0eef30a1aaa0b704f54b50e7280d77a5979e2
Adrián Sandoval <Bichito2509@gmail.com>
2025-12-04 13:01:46 +0100

    Hecho ejercicios de entradas en clase

--- Unidad3/src/ejercicios/Entradas.java
+++ Unidad3/src/ejercicios/Entradas.java
@@ -2,4 +2,34 @@ package ejercicios;
 
 public class Entradas {
 
+	private String nombre;
+	private int numEntradas;
+	private int precio;
+
+	public Entradas(String nombre, int numEntradas, int precio) {
+		this.nombre = nombre;
+		this.numEntradas = numEntradas;
+		this.precio = precio;
+	}
+
+	public void vender() {
+		int escogidas;
+
+	}
+
+	public void mostrarEntradas() {
+		System.out.println("Quedan " + numEntradas + " entradas");
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getNumEntradas() {
+		return numEntradas;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
 }
--- Unidad3/src/ejercicios/EntradasMain.java
+++ Unidad3/src/ejercicios/EntradasMain.java
@@ -1,10 +1,77 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class EntradasMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int respuesta, respuesta2;
+		Scanner sc = new Scanner(System.in);
+		Entradas fsur = new Entradas("Fondo Sur", 500, 10);
+		Entradas fnorte = new Entradas("Fondo Norte", 700, 10);
+		Entradas palta = new Entradas("Preferencia Alta", 300, 25);
+		Entradas pbaja = new Entradas("Preferencia Baja", 250, 40);
+
+		do {
+			System.out.println("--------------");
+			System.out.println("-----MENÚ-----");
+			System.out.println("1. Mostrar número de entradas libres que hay en cada zona");
+			System.out.println("2. Mostrar precio por entrada en cada zona");
+			System.out.println("3. Vender entradas");
+			System.out.println("4. Salir");
+			respuesta = sc.nextInt();
+
+			switch (respuesta) {
+			case 1:
+				System.out.println("--------------");
+				System.out.println("----QUEDAN----");
+				System.out.println("1. Fondo Norte");
+				System.out.println("2. Fondo Sur");
+				System.out.println("3. Preferencia Alta");
+				System.out.println("4. Preferencia Baja");
+				respuesta2 = sc.nextInt();
+				
+				switch (respuesta2) {
+				case 1:
+					fnorte.mostrarEntradas();
+					break;
+				case 2:
+					fsur.mostrarEntradas();
+					break;					
+				case 3:
+					palta.mostrarEntradas();
+					break;					
+				case 4:
+					pbaja.mostrarEntradas();
+					break;					
+				default:
+					System.err.println("Número inválido compañerete");
+					break;
+				}
+				break;
+
+			case 2:
+				System.out.println("--------------");
+				System.out.println("----PRECIO----");
+				System.out.println("1. Fondo Norte: 10€");
+				System.out.println("2. Fondo Sur: 10€");
+				System.out.println("3. Preferencia Alta: 25€");
+				System.out.println("4. Preferencia Baja: 40€");
+
+				break;
+				
+			case 3:
+				
+				break;
+				
+			default:
+				break;
+			}
+			
+		} while (respuesta != 4);
+		
+		System.out.println("Gracias por usar el gestor de entradas del Gipsy Colliseum");
 	}
 
 }
