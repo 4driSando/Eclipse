cd2c40cb512d3df9be778f94038af1b741e0c705
Adrián Sandoval <Bichito2509@gmail.com>
2025-12-15 22:00:16 +0100

    1er ejercicio de las librerías terminado

--- Unidad4/src/ejercicios/Ejercicio01_U04.java
+++ Unidad4/src/ejercicios/Ejercicio01_U04.java
@@ -6,10 +6,18 @@ public class Ejercicio01_U04 {
 
 	public static void main(String[] args) {
 
-		int num = 8;
-		
-		Clase40.imprimePares(num);
-		Clase40.tablaMultiplicar(num);
+		int num1 = 20, num2 = 8, num3 = 10, ancho = 10, alto = 5, dia = 25, mes = 9, anio = 2025;
+
+		Clase40.imprimePares(num1);
+		System.out.println("==========");
+		Clase40.tablaMultiplicar(num2);
+		System.out.println("==========");
+		Clase40.mostrarFibonacci(num3);
+		System.out.println("==========");
+		System.out.println("El área del rectángulo es " + Clase40.areaRectangulo(ancho, alto));
+		System.out.println("==========");
+		System.out.println("La fecha es " + Clase40.esFechaCorrecta(dia, mes, anio));
+
 	}
 
 }
--- Unidad4/src/ejercicios/morrocodutils/Clase40.java
+++ Unidad4/src/ejercicios/morrocodutils/Clase40.java
@@ -4,29 +4,68 @@ public class Clase40 {
 
 	int num;
 
-	public static void imprimePares(int num) {
-		if (num % 2 == 0) {
+	public static void imprimePares(int num1) {
+		if (num1 % 2 == 0) {
 			do {
-				num -= 2;
-				System.out.println("- " + num);
-			} while (num >= 2);
+				num1 -= 2;
+				System.out.println("- " + num1);
+			} while (num1 >= 2);
 
-		} else if (num == 2 || num == 3) {
+		} else if (num1 == 2 || num1 == 3) {
 			System.out.println("- 1");
 
 		} else {
-			num -= 1;
-			System.out.println("- " + num);
+			num1 -= 1;
+			System.out.println("- " + num1);
 			do {
-				num -= 2;
-				System.out.println("- " + num);
-			} while (num >= 2);
+				num1 -= 2;
+				System.out.println("- " + num1);
+			} while (num1 >= 2);
 		}
 	}
-	
-	public static void tablaMultiplicar(int num) {
-		for(int i = 1; i < 11; i++) {
-			System.out.println(i + " x " + num + " = " + (num*i));
+
+	public static void tablaMultiplicar(int num2) {
+		for (int i = 1; i < 11; i++) {
+			System.out.println(i + " x " + num2 + " = " + (num2 * i));
 		}
 	}
+
+	public static void mostrarFibonacci(int num3) {
+		int pnum = 0, snum = 1, numveces = 0;
+
+		do {
+			System.out.println(pnum);
+			int suma = pnum + snum;
+			pnum = snum;
+			snum = suma;
+			numveces++;
+		} while (numveces < num3);
+	}
+
+	public static double areaRectangulo(double ancho, double alto) {
+		return ancho * alto;
+	}
+
+	public static boolean esFechaCorrecta(int dia, int mes, int anio) {
+		boolean correcto = false;
+		int maxDias;
+
+		if (anio > 0 && mes >= 1 && mes <= 12 && dia >= 1) {
+			if (mes == 2) {
+				if ((anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0))) {
+					maxDias = 29;
+				} else {
+					maxDias = 28;
+				}
+			} else {
+				maxDias = 30;
+			}
+
+			if (dia <= maxDias) {
+				correcto = true;
+			}
+		}
+		return correcto;
+	}
+
 }
