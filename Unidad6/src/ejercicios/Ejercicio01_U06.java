f12f52451d5f8e3064a72f9f40fb87519ec6c2fd
Adrián Sandoval <Bichito2509@gmail.com>
2026-01-20 01:57:09 +0100

    Útiles de ArrayList terminados

--- Unidad6/src/ejercicios/Ejercicio01_U06.java
+++ Unidad6/src/ejercicios/Ejercicio01_U06.java
@@ -1,10 +1,27 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import ejercicios.morrocodutils.SandoArrayLists;
+
 public class Ejercicio01_U06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ArrayList<Integer> numeros = SandoArrayLists.generaArrayList(10, 1, 10);
+		ArrayList<Integer> alReves = SandoArrayLists.volteaArray(numeros);
+		int min = SandoArrayLists.minimoArrayList(numeros);
+		int max = SandoArrayLists.maximoArrayList(numeros);
+		double media = SandoArrayLists.mediaArray(numeros);
+		int buscar = 5;
+		boolean esta = SandoArrayLists.estaEnArray(numeros, buscar);
+		int pos = SandoArrayLists.posicionEnArray(numeros, buscar);
+
+		System.out.println("ArrayList generado: " + numeros);
+		System.out.println("Mínimo: " + min);
+		System.out.println("Máximo: " + max);
+		System.out.println("Media: " + media);
+		System.out.println("¿Está el " + buscar + " en el arrayList? " + esta);
+		System.out.println("Posición del " + buscar + ": " + pos);
+		System.out.println("ArrayList volteado: " + alReves);
 	}
-
 }
--- Unidad6/src/ejercicios/morrocodutils/SandoArrayLists.java
+++ Unidad6/src/ejercicios/morrocodutils/SandoArrayLists.java
@@ -13,13 +13,13 @@ public class SandoArrayLists {
 	 * @return devuelve array creado y rellenado con enteros random
 	 */
 	public static ArrayList<Integer> generaArrayList(int num, int min, int max) {
-		ArrayList<Integer> arrayList1 = new ArrayList<>();
-		int aleatorio = (int) (Math.random() * (max - min + 1)) + min;
+	    ArrayList<Integer> arrayList1 = new ArrayList<>();
 
-		for (int i = 0; i < num; i++) {
-			arrayList1.add(aleatorio);
-		}
-		return arrayList1;
+	    for (int i = 0; i < num; i++) {
+	        int aleatorio = (int) (Math.random() * (max - min + 1)) + min;
+	        arrayList1.add(aleatorio);
+	    }
+	    return arrayList1;
 	}
 
 	/**
@@ -88,5 +88,70 @@ public class SandoArrayLists {
 
 		return media;
 	}
+	
+	public static double mediaArrayDouble(ArrayList<Double> arrayList) {
+		double media, suma = 0;
+
+		for (int i = 0; i < arrayList.size(); i++) {
+			suma += arrayList.get(i);
+		}
+		media = suma / arrayList.size();
+
+		return media;
+	}
+	
+	/**
+	 * Comprueba si el número introducido está o no en el arrayList
+	 * 
+	 * @param arrayList
+	 * @param num   = número que queremos comprobar
+	 * @return devuelve true o false
+	 */
+	public static boolean estaEnArray(ArrayList<Integer> arrayList, int num) {
+		boolean estaEn = false;
+
+		for (int i = 0; i < arrayList.size(); i++) {
+			if (num == arrayList.get(i)) {
+				estaEn = true;
+			}
+		}
+		return estaEn;
+	}
+	
+	/**
+	 * Comprueba las posiciones del arrayList
+	 * 
+	 * @param arrayList
+	 * @param num   = número que queremos saber en qué posición está
+	 * @return devuelve la posición donde se encuentra el número introducido
+	 */
+	public static int posicionEnArray(ArrayList<Integer> arrayList, int num) {
+		int posicion = -1;
+
+		for (int i = 0; i < arrayList.size(); i++) {
+			if (num == arrayList.get(i)) {
+				posicion = i + 1;
+			}
+		}
+		return posicion;
+	}
+	
+	/**
+	 * Voltea el arrayList introducido
+	 * 
+	 * @param arrayList
+	 * @return devuelve otro array pero invertido
+	 */
+	public static ArrayList<Integer> volteaArray(ArrayList<Integer> lista) {
+	    ArrayList<Integer> yarraList = new ArrayList<>();
+
+	    for (int i = lista.size() - 1; i >= 0; i--) {
+	        yarraList.add(lista.get(i));
+	    }
+
+	    return yarraList;
+	}
+	
+
 
 }
