6e10df9f8bd32e96815047645b92ef92d9ba0254
Adrián Sandoval <Bichito2509@gmail.com>
2026-03-04 10:22:02 +0100

    Expresiones regulares

---
+++ Unidad8/src/ejercicios/PruebasExpresiones.java
@@ -0,0 +1,63 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class PruebasExpresiones {
+
+	public static void main(String[] args) {
+
+		String respuesta;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Escribe una palabra entre 5 y 8 letras");
+		respuesta = sc.nextLine();
+		if (respuesta.matches("\\w{5,8}")) {
+			System.out.println("Correcto");
+		} else {
+			System.out.println("Incorrecto");
+		}
+		
+		System.out.println("3 letras, 5 números y un guión");
+		respuesta = sc.nextLine();
+		if (respuesta.matches("[a-zA-Z]+,[0-9]+-")) {
+			System.out.println("Correcto");
+		} else {
+			System.out.println("Incorrecto");
+		}
+		
+		System.out.println("Comprobar que contenga hola u Hola");
+		respuesta = sc.nextLine();
+		if (respuesta.matches("hola.+")) {
+			System.out.println("Hola u hola detectado");
+		} else {
+			System.out.println("Incorrecto");
+		}
+		
+		System.out.println("Comprobar que no hay ni hola, ni Hola");
+		respuesta = sc.nextLine();
+		if (!respuesta.matches("hola.+") || !respuesta.matches("Hola.+")) {
+			System.out.println("Hola u hola detectado");
+		} else {
+			System.out.println("Incorrecto");
+		}
+			
+		System.out.println("Comprobar hola 3 veces");
+		respuesta = sc.nextLine();
+		if (respuesta.matches("(hola)*{3}")) {
+			System.out.println("Correcto");
+		} else {
+			System.out.println("Incorrecto");
+		}
+		
+		System.out.println("Comprobar que empiece por hola y acabe por Pepe");
+		respuesta = sc.nextLine();
+		if (respuesta.matches("^hola.*Pepe$")) {
+			System.out.println("Correcto");
+		} else {
+			System.out.println("Incorrecto");
+		}
+		sc.close();
+	}
+
+}
