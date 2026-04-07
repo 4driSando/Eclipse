3abdf463f268da4f245acadb90fb8837129433c2
Adrián Sandoval <Bichito2509@gmail.com>
2026-04-07 10:08:38 +0200

    Puto ejercicio 9

--- Unidad9/src/ejercicios/Ejercicio09_U09.java
+++ Unidad9/src/ejercicios/Ejercicio09_U09.java
@@ -1,33 +1,46 @@
 package ejercicios;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.TreeMap;
 
 public class Ejercicio09_U09 {
 
 	public static void main(String[] args) {
 
-		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader("Documentos/alumnos_notas.txt"));
+			BufferedReader br = new BufferedReader(new FileReader("Documentos/alumnos_notas.txt"));
+			TreeMap<String, Double> alumnos = new TreeMap<String, Double>();
 			String linea;
-			String[] Alumnos = new String[10];
-			
+
 			while ((linea = br.readLine()) != null) {
 				
-			}
-
-			 
-		} catch (Exception e) {
+				String[] lineaActual = linea.split(" ");
+				
+				String nombre = lineaActual[0] + " " + lineaActual[1];
+				System.out.println(nombre);
+				
+				int totalNotas = 0;
+				double media = 0;
+				
+				for (int i = 2; i < lineaActual.length; i++) {
+					media += Integer.parseInt(lineaActual[i]);
+					totalNotas++;
+				}
+				
+				media /= totalNotas;
+				
+				alumnos.put(nombre, media);
+								
+			}						
+			br.close();			
+			
+		} catch (FileNotFoundException fnfe) {
 			System.err.println("Error leyendo el archivo");
-		} finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			}
+		} catch (IOException ioe) {
+			System.out.println("No se puede leer el archivo");
 		}
 	}
-
 }
