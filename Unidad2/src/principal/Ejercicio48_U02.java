28ae68f5f6d8f2e5873f504ff31c9d4aa5f6a00a
Adrián Sandoval <Bichito2509@gmail.com>
2026-04-30 10:23:42 +0200

    No sé, cambios de héctor

--- Unidad2/src/principal/Ejercicio48_U02.java
+++ Unidad2/src/principal/Ejercicio48_U02.java
@@ -5,21 +5,20 @@ import java.util.*;
 public class Ejercicio48_U02 {
 
 	public static void main(String[] args) {
-
-		int[] num = new int[10];
 		Scanner sc = new Scanner(System.in);
-		
-		
+		int[] num = new int[10];
+
 		System.out.println("Introduce 10 números para mostrarlos en orden inverso");
 		for(int i = 0; i < num.length; i++) {
-			System.out.println("Nº " +i);
+			System.out.println("Número " +i + ": ");
 			num[i] = sc.nextInt();
 		}
-				
-		System.out.print("| ");
-		
-		for (int j = num.length - 1; j >= 0; j--) {
-			System.out.print(num[j] + " | ");
+
+		for(int i = num.length - 1; i >= 0; i--) {
+			System.out.print(num[i] + " | ");
 		}
+		
+		sc.close();
+		
 	}
 }
