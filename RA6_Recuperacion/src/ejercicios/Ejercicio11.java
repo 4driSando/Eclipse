c6c7fb573c88388038f89e8fe174c8db30f98b35
Adrián Sandoval <Bichito2509@gmail.com>
2026-05-06 10:15:48 +0200

    Ejercicio11 terminado

--- RA6_Recuperacion/src/ejercicios/Ejercicio11.java
+++ RA6_Recuperacion/src/ejercicios/Ejercicio11.java
@@ -1,21 +1,14 @@
 package ejercicios;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Ejercicio11 {
 
 	public static void main(String[] args) {
 
-		// Implementa el control de acceso al área restringida de un programa. Se debe
-		// pedir un nombre de usuario y una contraseña. Si el usuario introduce los
-		// datos correctamente, el programa dirá “Ha accedido al área restringida”. El
-		// usuario tendrá un máximo de 3 oportunidades. Si se agotan las oportunidades
-		// el programa dirá “Lo siento, no tiene acceso al área restringida”. Los
-		// nombres de usuario con sus correspondientes contraseñas deben estar
-		// almacenados en una estructura de la clase HashMap.
-
-		HashMap<String, String> area = new HashMap<String, String>();
+		HashMap<String, String> mapa = new HashMap<String, String>();
 		Scanner sc = new Scanner(System.in);
 		int respuesta;
 
@@ -34,46 +27,87 @@ public class Ejercicio11 {
 				String username = sc.next();
 				System.out.print("Contraseña: ");
 				String psswd = sc.next();
-				area.put(psswd, username);
+				mapa.put(psswd, username);
 				break;
 
 			case 2:
-				// Arreglar esto
 				int contador = 0;
 				do {
-				System.out.print("Nombre de usuario: ");
-				String usernamePrueba = sc.next();
-					if (area.containsValue(usernamePrueba)) {
+					System.out.print("Nombre de usuario: ");
+					String usernamePrueba = sc.next();
+					if (mapa.containsValue(usernamePrueba)) {
 						int contador2 = 0;
 						do {
 							System.out.print("Contraseña: ");
 							String psswdPrueba = sc.next();
-							if (area.containsKey(psswdPrueba)) {
+							if (mapa.containsKey(psswdPrueba)) {
 								System.out.println("Acceso permitido!");
+								break;
 							} else {
 								System.err.println("Contraseña Incorrecta, vuelva a intentarlo");
 								contador2++;
 							}
-
-						} while (contador2 < 3);
+						} while (contador2 != 3);
+						break;
+					} else {
+						System.err.println("Nombre de usuario no encontrado\n");
+						contador++;
+						if (contador == 3) {
+							System.err.println("Límite de errores alcanzado");
+						}
 					}
-				} while (contador < 3);
-
+				} while (contador != 3);
 				break;
 
 			case 3:
+				System.out.println("\n");
+				int i = 1;
+				for (Entry<String, String> entry : mapa.entrySet()) {
+					System.out.println(i + ". " + entry.getValue());
+					i++;
+				}
+				int contador1 = 0;
+				do {
+					System.out.println("Usuario a eliminar: ");
+					String userRmv = sc.next();
+					if (mapa.containsValue(userRmv)) {
+						int contador2 = 0;
+						do {
+							System.out.print("Contraseña: ");
+							String psswdPrueba = sc.next();
+							if (mapa.containsKey(psswdPrueba)) {
+								System.out.println("Usuario eliminado correctamente!");
+								mapa.remove(psswdPrueba);
+								break;
+							} else {
+								System.err.println("Contraseña Incorrecta, vuelva a intentarlo");
+								contador2++;
+							}
+						} while (contador2 != 3);
+						break;
+					} else {
+						System.err.println("Nombre de usuario no encontrado\n");
+						contador1++;
+						if (contador1 == 3) {
+							System.err.println("Límite de errores alcanzado");
+						}
+					}
+				} while (contador1 != 3);
 
 				break;
 
 			case 4:
-
+				System.out.println("Gracias por usar el gestor de cuentas morrocotudo!\nSaliendo...");
 				break;
 
 			default:
+				System.out.println("Opción no válida");
 				break;
 			}
 
 		} while (respuesta != 4);
+
+		sc.close();
 
 	}
 
