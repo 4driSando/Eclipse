5c842fec63b7b8ea922b761386e48d660fb43dd1
Adrián Sandoval <Bichito2509@gmail.com>
2026-05-07 10:30:29 +0200

    + Pruebas unitarias

--- PruebasAdrianRepaso/src/app/App.java
+++ PruebasAdrianRepaso/src/app/App.java
@@ -1,6 +1,8 @@
 package app;
 
 import utils.Calculadora;
+import utils.Password;
+import utils.Tarifa;
 
 public class App {
 
@@ -17,7 +19,30 @@ public class App {
 		int potencia = calculadora.potencia();
 		int raiz = calculadora.raiz();
 		
+		System.out.println("CALCULADORA");
 		System.out.println("Suma: " + suma + "\nResta: " + resta + "\nProducto: " + producto +"\nDivisión: " + division + "\nPotencia: " + potencia + "\nRaiz: " + raiz);
+		
+		String psswd = "Lobatón";
+		String psswd2 = "Loba tón";
+		String psswd3 = "Lobatoide";
+		String psswd4 = "Lob@toide";
+		
+		System.out.println("\nCONTRASEÑA");
+		System.out.println("Son seguras las siguientes contraseñas?");
+		System.out.println(psswd + " => " + Password.esSegura(psswd));
+		System.out.println(psswd2 + " => " + Password.esSegura(psswd2));
+		System.out.println(psswd3 + " => " + Password.esSegura(psswd3));
+		System.out.println(psswd4 + " => " + Password.esSegura(psswd4));
+		
+		int edadCarla = 15;
+		int edadRuth = 45;
+		int edadGallego = 89;
+		System.out.println("\nTARIFA");
+		System.out.println("Carla paga " + Tarifa.calcularPrecioMensual(edadCarla));
+		System.out.println("Ruth paga " + Tarifa.calcularPrecioMensual(edadRuth));
+		System.out.println("El Gallego paga " + Tarifa.calcularPrecioMensual(edadGallego));
+
+
 
 	}
 
