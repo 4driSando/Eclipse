6dff517761295cc69b532072bdb3cbddf796411e
Adrián Sandoval <Bichito2509@gmail.com>
2026-05-14 12:39:42 +0200

    Añadido ejercicio 15

---
+++ RA6_Recuperacion/src/ejercicios/Ejercicio15.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class Ejercicio15 {
+
+	public static void main(String[] args) {
+		
+		// Crear un programa para gestionar el stock de productos en un almacén. Será
+		// necesario mantener información del nombre del producto, y las unidades del
+		// mismo. Se creará un menú que permita añadir productos, si el producto existe
+		// aumenta el stock, si no existe lo crea, eliminar productos, reducir el stock
+		// de un producto (teniendo en cuenta que no puede quedar stock negativo),
+		// buscar las unidades que hay de un determinado producto, listar todos los
+		// productos, y listar todos los productos junto con su stock.
+
+	}
+
+}
